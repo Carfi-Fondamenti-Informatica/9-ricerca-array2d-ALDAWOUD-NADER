8b40a239bfbe0ed841df65158c0ffdfa86eafc00
ALDAWOUD-NADER <100731947+ALDAWOUD-NADER@users.noreply.github.com>
2022-05-14 20:08:55 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,25 @@
-
 #include <iostream>
 #include "lib.h"
 using namespace std;
 
 int main(){
-    int pos;
-    char a[10][20], f[1][20];
-    for (int i=0;i<10;i++){
-        cin>>a[i];
+    char nome[10][20];
+    char n[20];
+    init(nome);
+    init(n);
+
+    for (int i=0; i<10; i++){
+        cin >> nome[i];
     }
-    cin>>f[0];
-    if(ricerca(a,f,pos)){
-        cout<<pos<<endl;
-    }else{
+   
+    cin >> n;
+
+    int a= ricerca(nome,n);
+    if(a!=-1){
+        cout<<a;
+    } else{
         cout<<"non presente"<<endl;
     }
+
     return 0;
 }
