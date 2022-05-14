7f2cd53ce503dd0d9a5937a5e01f963f55eea11d
ALDAWOUD-NADER <100731947+ALDAWOUD-NADER@users.noreply.github.com>
2022-05-14 20:06:09 +0200

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,4 +1,3 @@
-void inserimento(char mat[10][20]);
-void inserimento(char nome[20]);
-bool strcmp(char string1[20], char string[20]);
-int confronto(char mat[10][20], char nome[20]);
+void init(char [10][20]);
+void init(char [20]);
+int ricerca(char [10][20], char [20]);
