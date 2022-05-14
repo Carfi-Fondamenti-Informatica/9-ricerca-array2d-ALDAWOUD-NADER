4f16fc8d5b1ed92f86d2375b9587e3c121e72bb3
ALDAWOUD-NADER <100731947+ALDAWOUD-NADER@users.noreply.github.com>
2022-05-14 19:58:02 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,33 @@
+#include "lib.h"
+
+void init(char mat[][20], int dim){
+    for(int i=0; i<dim; i++){
+        for(int j=0; j<20; j++){
+            mat[i][j]=0;
+        }
+    }
+}
+
+void init(char ar[], int dim){
+    for(int i=0; i<dim; i++){
+        ar[i]=0;
+    }
+}
+
+int ricerca_nomi(char elenco_nomi[10][20], char nomi[20]){
+    int a;
+    for(int i=0; i<10; i++){
+        a=0;
+        for(int j=0; j<20; j++){
+            if(elenco_nomi[i][j]==nomi[j]){
+                a++;
+            }else
+                break;
+        } 
+        if(a==20){
+           return i;
+        }
+    }
+    return -1;
+}
 
